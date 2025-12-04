58a083f02c006d10c5053039336c187111e1fcdd
Alireza Fadaei <a.fadaei@fardaadco.ir>
2025-12-05 01:27:30 +0330

    feat(code): docusaurus configs has been updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,7 +28,7 @@ const config = {
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
     organizationName: 'aliz-f', // Usually your GitHub org/user name.
-    projectName: 'aliz-net.github.io', // Usually your repo name.
+    projectName: 'aliznet.github.io', // Usually your repo name.
     trailingSlash: false,
     deploymentBranch: 'main',
     onBrokenLinks: 'throw',
