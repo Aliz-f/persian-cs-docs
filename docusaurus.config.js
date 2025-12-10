55aca83175e609fc5519cb26b6314f4410144f03
Alireza Fadaei <a.fadaei@fardaadco.ir>
2025-12-10 15:42:34 +0330

    feat(search): change search engine from algolia to typesense

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -90,44 +90,67 @@ const config = {
             noModule: true,
         },
     ],
-
+    themes: ['docusaurus-theme-search-typesense'],
     themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
         ({
-            algolia: {
-                // The application ID provided by Algolia
-                appId: 'L1W3D7Q9LB',
+            // algolia: {
+            //     // The application ID provided by Algolia
+            //     appId: 'L1W3D7Q9LB',
+            //
+            //     // Public API key: it is safe to commit it
+            //     apiKey: '19db63d936d2f7d50c7f798f6cd582c9',
+            //
+            //     indexName: 'AlizNet',
+            //
+            //     // Optional: see doc section below
+            //     contextualSearch: true,
+            //
+            //     // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+            //     externalUrlRegex: 'external\\.com|domain\\.com',
+            //
+            //     // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+            //     // replaceSearchResultPathname: {
+            //     //     from: '/docs/', // or as RegExp: /\/docs\//
+            //     //     to: '/docs/',
+            //     // },
+            //
+            //     // Optional: Algolia search parameters
+            //     searchParameters: {},
+            //
+            //     // Optional: path for search page that enabled by default (`false` to disable it)
+            //     searchPagePath: 'search',
+            //
+            //     // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+            //     insights: false,
+            //
+            //     // Optional: whether you want to use the new Ask AI feature (undefined by default)
+            //     askAi: 'YOUR_ALGOLIA_ASK_AI_ASSISTANT_ID',
+            //
+            //     //... other Algolia params
+            // },
+            typesense: {
+                typesenseCollectionName: 'aliznet',
+                typesenseServerConfig: {
+                    nodes: [
+                        {
+                            host: 'search.aliznet.ir',
+                            port: 443,
+                            protocol: 'https',
+                        },
+                    ],
+                    apiKey: 'llYm1yTg89bKEC02MA21',
+                },
 
-                // Public API key: it is safe to commit it
-                apiKey: '19db63d936d2f7d50c7f798f6cd582c9',
+                // Optional: Typesense search parameters: https://typesense.org/docs/0.24.0/api/search.html#search-parameters
+                typesenseSearchParameters: {
+                    query_by:
+                        'hierarchy.lvl0,hierarchy.lvl1,hierarchy.lvl2,hierarchy.lvl3,hierarchy.lvl4,hierarchy.lvl5,hierarchy.lvl6,content,embedding',
+                    vector_query: 'embedding:([], k: 5, distance_threshold: 1.0, alpha: 0.2)'
+                },
 
-                indexName: 'AlizNet',
-
-                // Optional: see doc section below
+                // Optional
                 contextualSearch: true,
-
-                // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-                externalUrlRegex: 'external\\.com|domain\\.com',
-
-                // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-                // replaceSearchResultPathname: {
-                //     from: '/docs/', // or as RegExp: /\/docs\//
-                //     to: '/docs/',
-                // },
-
-                // Optional: Algolia search parameters
-                searchParameters: {},
-
-                // Optional: path for search page that enabled by default (`false` to disable it)
-                searchPagePath: 'search',
-
-                // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
-                insights: false,
-
-                // Optional: whether you want to use the new Ask AI feature (undefined by default)
-                askAi: 'YOUR_ALGOLIA_ASK_AI_ASSISTANT_ID',
-
-                //... other Algolia params
             },
 
             // Replace with your project's social card
